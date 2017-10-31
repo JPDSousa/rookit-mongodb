2638d0fd688795c24a51209f7a3addf0fff1b322
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-31 09:23:39 +0000

    Update Filters

---
+++ src/main/java/org/rookit/mongodb/queries/filter/GenreFilter.java
@@ -0,0 +1,14 @@
+package org.rookit.mongodb.queries.filter;
+
+import java.util.regex.Pattern;
+
+@SuppressWarnings("javadoc")
+public interface GenreFilter<Q> extends PlayableFilter<Q> {
+	
+	Q withName(String genreName);
+	Q withName(Pattern regex);
+
+	Q withDescription(String description);
+	Q withDescription(Pattern regex);
+
+}
---
+++ src/main/java/org/rookit/mongodb/queries/filter/TrackFilter.java
@@ -0,0 +1,40 @@
+package org.rookit.mongodb.queries.filter;
+
+import java.util.regex.Pattern;
+
+import org.bson.types.ObjectId;
+import org.rookit.dm.artist.Artist;
+import org.rookit.dm.track.Track;
+
+@SuppressWarnings("javadoc")
+public interface TrackFilter<Q> extends GenreableFilter<Q> {
+	
+	Q withTitle(String title);
+	Q withTitle(Pattern regex);
+	
+	Q withMainArtist(Artist artist);
+	
+	Q withFeature(Artist artist);
+	
+	Q withProducer(Artist artist);
+	
+	Q withBPM(int bpm);
+	Q withBPM(int min, int max);
+	
+	Q withLyrics(String lyrics);
+	Q withLyrics(Pattern regex);
+	
+	Q withExplicitLyrics(boolean explicit);
+	
+	Q withOriginal(Track track);
+	Q withOriginal(ObjectId id);
+
+	Q withHiddenTrack(String hiddenTrack);
+	Q withHiddenTrack(Pattern regex);
+
+	Q withVersionArtist(Artist artist);
+	
+	Q withVersionToken(String token);
+	Q withVersionToken(Pattern regex);
+
+}
